3f656ade9767f7f5cc96deb2bf2136c9f6a3e89a
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-11 17:04:05 +0200

    added variable numero

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@
 let studente = prompt("Inserisci il tuo Nome");
 let cognome = prompt("Inserisci il tuo Cognome");
 let colorePreferito = prompt("Qual'è il tuo colore preferito?");
+let numero = parseInt(prompt("Inserisci il tuo numero preferito"));
 
-document.getElementById("titolo").innerHTML = studente + cognome + colorePreferito + 99;
+document.getElementById("titolo").innerHTML = studente + cognome + colorePreferito + numero;
 
 console.log(studente);
 console.log(cognome);
 console.log(colorePreferito);
+console.log(numero);
